5293858f44bef0863cb4a5012f3fe9695c9875d7
htafolla <htafolla@users.noreply.github.com>
2023-12-21 14:10:49 +0000

    deploy: 4fe29bdc0d210871d8973dc374e42de46fee8e6c

---
+++ precache-manifest.e3106764b7a9e7dd27332ba222ebc3a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44630b6b5103ab8ea16eed6510a0b857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3029db429f089b29357d",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1ee65845d915bfac6a8a",
+    "url": "/static/js/2.af88aed4.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.af88aed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c5b615626fbb1e440e",
+    "url": "/static/js/3.920636fc.chunk.js"
+  },
+  {
+    "revision": "3029db429f089b29357d",
+    "url": "/static/js/main.3e576347.chunk.js"
+  },
+  {
+    "revision": "b714ce127e193fcf5647",
+    "url": "/static/js/runtime-main.16f02cb4.js"
+  }
+]);
